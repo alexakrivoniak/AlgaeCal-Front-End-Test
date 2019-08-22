de0e9b2b663c0bc061a977900b225a23521f0b32
alexakrivoniak <alexa@bluelikeneon.com>
2019-08-21 21:58:11 -0400

    Task 1 complete

--- src/index.js
+++ src/index.js
@@ -6,4 +6,35 @@ import 'bootstrap';
 
 document.addEventListener('DOMContentLoaded', () => {
   loadSVGs();
+  tallestProductBox();
 });
+
+videoOverlayPlay();
+percentOffBubble();
+
+function videoOverlayPlay() {
+	window._wq = window._wq || [];
+	_wq.push({ id: 'zwflowymel', onReady: function(video) {
+		jQuery('#play-button-overlay-zwflowymel').on('click', function() {
+			jQuery(this).fadeOut(500);
+		 	video.play();
+		});
+	}});
+
+}
+
+function percentOffBubble() {
+	$('.percent-off').each(function() {
+		if($(this).attr('data-total-savings-amount') == 0) {
+			$(this).hide();
+		}
+	});
+}
+
+function tallestProductBox() {
+	var productBoxesHeights = $('.product-box').map(function() {
+    	return $(this).height();
+  	}).get();
+  	var tallestHeight = Math.max.apply(null, productBoxesHeights);
+  	$('.product-box').height(tallestHeight);
+}
